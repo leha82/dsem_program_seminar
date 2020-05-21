84f356962718c1e9f4f56e529feea061f8f83cbf
leesangmoo <dbrrns333@naver,com>
2020-05-21 21:55:31 +0900

    #2

--- src/maze/Maze.java
+++ src/maze/Maze.java
@@ -1,9 +1,11 @@
 package maze;
 
+import java.io.*;
+
 public class Maze {
 	private int width, height;
 	private int[][] map;
-	
+	private String[] token;
 	public Maze() {
 		this(0,0,null);
 	}
@@ -15,11 +17,37 @@ public class Maze {
 	}
 	
 	public Maze(String filename) {
+		map = new int[7][7];
+		try {
+			File file = new File(filename);
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String line = "";
+			line = br.readLine();
+			int row = 0;
+
+			while ((line = br.readLine()) != null) {
+				token = line.split(" ");
+				for (int i = 0; i < token.length; i++) {
+					map[row][i] = Integer.parseInt(token[i]);
+				}
+				for (int i = 0; i < token.length; i++) {
+					System.out.print(map[row][i]);
+				}
+				System.out.println(" ");
+				row++;
+			}
+			this.width =token.length;
+			this.height = token.length;
+			this.map = map;	
+			br.close();
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		// file에서 map 읽어오기 #2
 		
-//		this.width = width;
-//		this.height = height;
-//		this.map = map;
 	}
 	
 	public int[][] getArea(int x, int y) {
