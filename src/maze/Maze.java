2f719b49b4ef4d1b8a73e6bd6c41256e976f1bb7
wkddnfls <wkddnfls@gmail.com>
2020-05-21 20:39:33 +0900

    #3 코드

--- src/maze/Maze.java
+++ src/maze/Maze.java
@@ -25,6 +25,7 @@ public class Maze {
 	public int[][] getArea(int x, int y) {
 		int[][] smap = new int[3][3];
 		// 지도에서 해당 위치 주변의 9칸을 배열로 만들어 리턴한다. #3
+		
 		int[] index = {-1, 0, 1};
 		
 		for (int i=0;i<smap.length;i++) {
