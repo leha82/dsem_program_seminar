404d1f63b640b6a1e963765feaaca541dd1d7416
sojin0319 <thdus10725@gmail.com>
2020-06-11 19:25:22 +0900

    commit

---
+++ src/mice/Mouse_sojin.java
@@ -0,0 +1,69 @@
+package mice;
+
+public class Mouse_sojin extends Mouse {
+	int mouse_dir = 2;
+	
+	@Override
+	public int nextMove(int x, int y, int[][] smap) {
+		int dir = 0;
+		if (mouse_dir == 2) {
+			if (smap[2][1] == 0) {
+				dir = 3;
+				mouse_dir = 3;
+			} else if (smap[1][2] == 0) {
+				dir = 2;
+				mouse_dir = 2;
+			} else if (smap[0][1] == 0) {
+				dir = 1;
+				mouse_dir = 1;
+			} else if (smap[1][0] == 0) {
+				dir = 4;
+				mouse_dir = 4;
+			}
+		} else if (mouse_dir == 3) {
+			if (smap[1][0] == 0) {
+				dir = 4;
+				mouse_dir = 4;
+			} else if (smap[2][1] == 0) {
+				dir = 3;
+				mouse_dir = 3;
+			} else if (smap[1][2] == 0) {
+				dir = 2;
+				mouse_dir = 2;
+			} else if (smap[0][1] == 0) {
+				dir = 1;
+				mouse_dir = 1;
+			}
+		} else if (mouse_dir == 4) {
+			if (smap[0][1] == 0) {
+				dir = 1;
+				mouse_dir = 1;
+			} else if (smap[1][0] == 0) {
+				dir = 4;
+				mouse_dir = 4;
+			} else if (smap[2][1] == 0) {
+				dir = 3;
+				mouse_dir = 3;
+			} else if (smap[1][2] == 0) {
+				dir = 2;
+				mouse_dir = 2;
+			}
+		} else if (mouse_dir == 1) {
+			if (smap[1][2] == 0) {
+				dir = 2;
+				mouse_dir = 2;
+			} else if (smap[0][1] == 0) {
+				dir = 1;
+				mouse_dir = 1;
+			} else if (smap[1][0] == 0) {
+				dir = 4;
+				mouse_dir = 4;
+			} else if (smap[2][1] == 0) {
+				dir = 3;
+				mouse_dir = 3;
+			}
+		}
+
+		return dir;
+	}
+}
