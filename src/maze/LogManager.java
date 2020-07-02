2d888b9eb14b27970dbb2935762f5cf638a90997
leha82 <leha82@kunsan.ac.kr>
2020-07-02 18:06:44 +0900

    데이터베이스 연동 및 실행 가늫한 버전

--- src/maze/LogManager.java
+++ src/maze/LogManager.java
@@ -5,23 +5,23 @@ import java.sql.*;
 import java.text.SimpleDateFormat;
 
 public class LogManager {
-   private String driver;
-   private String url;
-   private String userid;
-   private String password;
-   
-   Connection conn;
-   Statement stmt;
-   ResultSet rs;
-   PreparedStatement pstmt;
-	
+	private String driver;
+	private String url;
+	private String userid;
+	private String password;
+
+	Connection conn;
+	Statement stmt;
+	ResultSet rs;
+	PreparedStatement pstmt;
+
 	public LogManager() {
 		this.driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
 		this.url = "jdbc:sqlserver://203.234.62.144:1433; databaseName = Maze";
 		this.userid = "sonyo";
 		this.password = "1234";
 	}
-	
+
 	public boolean connectDB() {
 		this.conn = null;
 		try {
@@ -34,127 +34,134 @@ public class LogManager {
 		}
 		return true;
 	}
-   public boolean disconnectDB() {
-      try {
-         if (this.stmt != null)
-            this.stmt.close();
-         if (this.pstmt != null)
-            this.pstmt.close();
-         if (this.conn != null)
-            this.conn.close();
-      } catch (Exception e) {
-         System.out.println(e.toString());
-         return false;
-      }
-      return true;
-   }
-   
-   public int getCount(String mouseName) {
-      int count = 0;
-      
-      // 데이터베이스 접속
-      connectDB();
-      
-      try {
-    	  
-         String sql = "select count from Log where mouse_name ='" + mouseName + "'"; 
-         pstmt = conn.prepareStatement(sql);
-           rs = pstmt.executeQuery();
-           
-           while(rs.next()) {
-              count = rs.getInt("count");
-           }
-           System.out.println(count);
-      } catch (SQLException e) {
-         e.printStackTrace();
-      }
-      
-      // 데이터베이스 접속 해제
-      disconnectDB();
-      return count;
-   }
-   
-   public ArrayList<String> getRankingList() {
-      ArrayList<String> rankList = new ArrayList<String>();
-      
-      // 데이터베이스 접속
-      connectDB();
 
-      // 테이블의 데이터 획득
+	public boolean disconnectDB() {
+		try {
+			if (this.stmt != null)
+				this.stmt.close();
+			if (this.pstmt != null)
+				this.pstmt.close();
+			if (this.conn != null)
+				this.conn.close();
+		} catch (Exception e) {
+			System.out.println(e.toString());
+			return false;
+		}
+		return true;
+	}
 
-      try {
-         String sql = "select * from Log"; 
-         pstmt = conn.prepareStatement(sql);
-           rs = pstmt.executeQuery();
-           
-           while(rs.next()) {
-              String id = rs.getString("Id");
-              String Mouse = rs.getString("mouse_name");
-              String timestamp= rs.getString("timestamp");
-              int count = rs.getInt("count");
-              rankList.add(id +"," + Mouse +"," + timestamp +"," + count);
+	public int getCount(String mouseName) {
+		int count = 0;
 
-           }
-           
-      } catch (SQLException e) {
-         e.printStackTrace();
-      }      // 데이터베이스 접속 해제
-      System.out.println(rankList);
-      disconnectDB();
-      return rankList;
-   }
+		// 데이터베이스 접속
+		connectDB();
 
-   public boolean putLog(String mouseName, int count) {
-      // 데이터베이스에 접속
-      connectDB();
-      
-      // 시스템에서 시간을 받아 datetime 유형으로 만든다.
-      // 테이블에 파라메터의 값을 넣는다
-      try {
-         SimpleDateFormat current_time = new SimpleDateFormat ( "yyyy-MM-dd HH:mm:ss");
-         String time_stamp = current_time.format (System.currentTimeMillis());
-         String sql = "insert into Log(mouse_name, timestamp, count) values ('" + mouseName + "','" + time_stamp + "', "+ count + ")";
-         stmt.executeLargeUpdate(sql);
-          
-      } catch (SQLException e) {
-         e.printStackTrace();
-         return false;
-      }   
-      // 데이터베이스 접속 해제
-      disconnectDB();
-      return true;
-   }
-   
-   public int getminCount(String mouseName) {
-	      int min_count = 0;
-	      
-	      // 데이터베이스 접속
-	      connectDB();
-	      
-	      try {
-	    	  
-	         String sql = "select min(count) as min_count from Log"; 
-	         pstmt = conn.prepareStatement(sql);
-	           rs = pstmt.executeQuery();
-	           
-	           while(rs.next()) {
-	              min_count = rs.getInt("min_count");
-	           }
-	           System.out.println(min_count);
-	      } catch (SQLException e) {
-	         e.printStackTrace();
-	      }
-	      
-	      // 데이터베이스 접속 해제
-	      disconnectDB();
-	      return min_count;
-	   }
-   
-   public static void main(String[] args) {
-      LogManager Log = new LogManager();
-      Log.putLog("woolin", 55);
-      Log.getminCount("Sunyoung_mouse");
-   }
-   
+		try {
+
+			String sql = "select count from Log where mouse_name ='" + mouseName + "'";
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				count = rs.getInt("count");
+			}
+//			System.out.println(count);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		// 데이터베이스 접속 해제
+		disconnectDB();
+		return count;
+	}
+
+	public ArrayList<String> getRankingList() {
+		ArrayList<String> rankList = new ArrayList<String>();
+
+		// 데이터베이스 접속
+		connectDB();
+
+		// 테이블의 데이터 획득
+
+		try {
+			String sql = "select * from Log order by count asc";
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				String id = rs.getString("id");
+				String Mouse = rs.getString("mouse_name");
+				String timestamp = rs.getString("timestamp");
+				int count = rs.getInt("count");
+				rankList.add(id + "," + Mouse + "," + timestamp + "," + count);
+
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} // 데이터베이스 접속 해제
+		System.out.println(rankList);
+		disconnectDB();
+		return rankList;
+	}
+
+	public boolean putLog(String mouseName, int count) {
+		boolean result = true;
+		
+		// 데이터베이스에 접속
+		connectDB();
+
+		// 시스템에서 시간을 받아 datetime 유형으로 만든다.
+		// 테이블에 파라메터의 값을 넣는다
+		try {
+			SimpleDateFormat current_time = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			String time_stamp = current_time.format(System.currentTimeMillis());
+			String sql = "insert into Log(mouse_name, timestamp, count) values ('" + mouseName + "','" + time_stamp
+					+ "', " + count + ")";
+//			stmt.executeLargeUpdate(sql);
+			stmt.executeUpdate(sql);
+
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			result = false;
+		}
+		
+		// 데이터베이스 접속 해제
+		disconnectDB();
+		return result;
+	}
+
+	public int getMinCount(String mouseName) {
+		int min_count = -1;
+
+		// 데이터베이스 접속
+		connectDB();
+
+		try {
+
+			String sql = "select count as min_count from Log "
+					+ "where mouse_name='" + mouseName + "';";
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				min_count = rs.getInt("min_count");
+			}
+			System.out.println(min_count);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		// 데이터베이스 접속 해제
+		disconnectDB();
+		return min_count;
+	}
+
+//	public static void main(String[] args) {
+//		LogManager Log = new LogManager();
+//		Log.putLog("woolin", 55);
+//		Log.getMinCount("Sunyoung_mouse");
+//	}
 
 }
--- src/maze/MazeEscapeGUI.java
+++ src/maze/MazeEscapeGUI.java
@@ -30,9 +30,8 @@ public class MazeEscapeGUI extends JFrame {
 	private int start_x, start_y;
 	private int curr_x, curr_y;
 	private int esc_x, esc_y;
-	private ArrayList<String> rankList;
 	private boolean finished;
-	
+
 	public MazeEscapeGUI() {
 		super("Maze Escape");
 		this.count = 0;
@@ -41,54 +40,52 @@ public class MazeEscapeGUI extends JFrame {
 
 	public void loadMap() {
 		this.maze = new Maze(mapfile);
-		
+
 		this.start_x = maze.getStart_x();
 		this.start_y = maze.getStart_y();
 		this.esc_x = maze.getEsc_x();
 		this.esc_y = maze.getEsc_y();
-		
+
 		this.curr_x = this.start_x;
 		this.curr_y = this.start_y;
 
 	}
-	
+
 	public void loadMice() {
 		miceList = new ArrayList<String>();
-		
+
 		// Todo : bin/mice 폴더안에 .class 파일명들을 리스트업하고 클래스명만 miceList로 넣기
 		// 패키지명.클래스명으로 list에 넣기 ex: mice.RandomMouse
-		
+
 		this.mouse = new RightHandMouse();
 	}
-	
+
 	public void initWindow() {
 		int[][] map = maze.getMap();
-		
-		
+
 		// Todo: 메뉴 추가 - Load Mouse - miceList를 나열
 		// mouse를 선택할 경우 - this.mouse에 해당 클래스 생성 및 지정
 		// mouse 선택 후 맵 초기화
-		
-		
+
 		mapPanel = new JPanel();
 		mapPanel.setLayout(new GridBagLayout());
 
 		mapLabels = new JLabel[map.length][];
-		
+
 		GridBagConstraints gbc = new GridBagConstraints();
-		
-		for (int i=0; i<map.length; i++) {
+
+		for (int i = 0; i < map.length; i++) {
 			mapLabels[i] = new JLabel[map[i].length];
-			
-			for (int j=0; j<map[i].length; j++) {
-				gbc.gridx=j;
-				gbc.gridy=i;
+
+			for (int j = 0; j < map[i].length; j++) {
+				gbc.gridx = j;
+				gbc.gridy = i;
 
 				if (curr_x == j && curr_y == i) {
 					mapLabels[i][j] = new JLabel(new ImageIcon("res/mouse.jpg"));
 				} else if (esc_x == j && esc_y == i) {
 					mapLabels[i][j] = new JLabel(new ImageIcon("res/goal.jpg"));
-				} else if (map[i][j]==1) {
+				} else if (map[i][j] == 1) {
 					mapLabels[i][j] = new JLabel(new ImageIcon("res/wall.jpg"));
 				} else {
 					mapLabels[i][j] = new JLabel(new ImageIcon("res/way.jpg"));
@@ -107,7 +104,7 @@ public class MazeEscapeGUI extends JFrame {
 				}
 			}
 		});
-		
+
 		btnNext10 = new JButton("다음 10번 이동");
 		btnNext10.addActionListener(new ActionListener() {
 			@Override
@@ -131,49 +128,15 @@ public class MazeEscapeGUI extends JFrame {
 		});
 		// 랭킹보기 버튼 추가
 		// 랭킹보기 버튼을 클릭하면 팝업창이 떠서 RankList를 출력한다.
-		class showrank extends  JFrame {
-			public showrank() {
-				LogManager log = new LogManager();
-				rankList = log.getRankingList();
-				String []column = {"id","mouse_name","timestamp","count"};
-				String [][]row = new String[rankList.size()][4];
-				for(int i =0;i<rankList.size();i++) {
-					String listline = rankList.get(i);
-					String[] line = listline.split(",");
-					for (int j =0;j <4; j++) {
-						row[i][j] = line[j];
-					}
-				}
-				setTitle("랭킹보기");
-				DefaultTableCellRenderer dtcr = new DefaultTableCellRenderer(); 
-			    dtcr.setHorizontalAlignment(SwingConstants.CENTER);
 
-				DefaultTableModel model = new DefaultTableModel(row,column);
-				JTable table = new JTable(model);
-				table.setRowHeight(25);
-				table.getColumnModel().getColumn(0).setPreferredWidth(20);
-				table.getColumnModel().getColumn(3).setPreferredWidth(20);
-				table.getColumnModel().getColumn(0).setCellRenderer(dtcr);
-				table.getColumnModel().getColumn(3).setCellRenderer(dtcr);
-				JScrollPane sc = new JScrollPane(table);
-				Container c = getContentPane();
-				String a1 = rankList.get(0);
-				ranking = new JLabel(a1);
-				c.add(ranking);
-				c.add(sc);
-				setSize(500, 600);
-				setVisible(true);
-			}
-		}
-		
 		showranking = new JButton("랭킹보기");
 		showranking.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				new showrank();
+				new ShowRanking();
 			}
 		});
-		
+
 		lbCount = new JLabel("이동횟수 : " + count);
 		infoPanel = new JPanel();
 		infoPanel.add(btnNext);
@@ -181,89 +144,127 @@ public class MazeEscapeGUI extends JFrame {
 		infoPanel.add(btnNextAll);
 		infoPanel.add(showranking);
 		infoPanel.add(lbCount);
-		
+
 		mainPanel = new JPanel();
 		mainPanel.setLayout(new BorderLayout());
-		mainPanel.add(mapPanel,"Center");
-		mainPanel.add(infoPanel,"South");
-		
+		mainPanel.add(mapPanel, "Center");
+		mainPanel.add(infoPanel, "South");
+
 		add(mainPanel);
-		
-		setSize (map[0].length * 60 + 100, map.length * 60 + 50);
+
+		setSize(map[0].length * 60 + 100, map.length * 60 + 50);
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-	
-	public void setWindow(int prev_x, int prev_y, int [][] map) {
+
+	public void setWindow(int prev_x, int prev_y, int[][] map) {
 		GridBagConstraints gbc = new GridBagConstraints();
-		
+
 		mapPanel.remove(mapLabels[prev_y][prev_x]);
 		mapLabels[prev_y][prev_x] = new JLabel(new ImageIcon("res/way.jpg"));
-		gbc.gridx=prev_x;
-		gbc.gridy=prev_y;
+		gbc.gridx = prev_x;
+		gbc.gridy = prev_y;
 		mapPanel.add(mapLabels[prev_y][prev_x], gbc);
 
 		mapPanel.remove(mapLabels[curr_y][curr_x]);
 		mapLabels[curr_y][curr_x] = new JLabel(new ImageIcon("res/mouse.jpg"));
-		gbc.gridx=curr_x;
-		gbc.gridy=curr_y;
+		gbc.gridx = curr_x;
+		gbc.gridy = curr_y;
 		mapPanel.add(mapLabels[curr_y][curr_x], gbc);
 
 		lbCount.setText("이동횟수 : " + count);
-		
+
 		revalidate();
 		repaint();
 	}
-	
+
 	public void play(int move) {
 		int[][] map = maze.getMap();
 		int prev_x = curr_x;
 		int prev_y = curr_y;
-		
-		int i=0;
+
+		int i = 0;
 		while (!finished && (i < move || move == -1)) {
-			int dir = mouse.nextMove(curr_x, curr_y, maze.getArea(curr_x, curr_y) );
-			
-			if (dir==1 && curr_y > 0) {
-				if (map[curr_y-1][curr_x]==0)
-					curr_y--;	
-			} else if (dir==2 && curr_x < maze.getWidth()-1) {
-				if (map[curr_y][curr_x+1]==0)
+			int dir = mouse.nextMove(curr_x, curr_y, maze.getArea(curr_x, curr_y));
+
+			if (dir == 1 && curr_y > 0) {
+				if (map[curr_y - 1][curr_x] == 0)
+					curr_y--;
+			} else if (dir == 2 && curr_x < maze.getWidth() - 1) {
+				if (map[curr_y][curr_x + 1] == 0)
 					curr_x++;
-			} else if (dir==3 && curr_y < maze.getHeight()-1) {
-				if (map[curr_y+1][curr_x]==0)
-					curr_y++;	
-			} else if (dir==4 && curr_x > 0) {
-				if (map[curr_y][curr_x-1]==0)
+			} else if (dir == 3 && curr_y < maze.getHeight() - 1) {
+				if (map[curr_y + 1][curr_x] == 0)
+					curr_y++;
+			} else if (dir == 4 && curr_x > 0) {
+				if (map[curr_y][curr_x - 1] == 0)
 					curr_x--;
 			}
-			
+
 			count++;
-			this.setWindow(prev_x, prev_y, map); 
+			this.setWindow(prev_x, prev_y, map);
 			prev_x = curr_x;
 			prev_y = curr_y;
 
 			if ((curr_x == this.esc_x) && (curr_y == this.esc_y)) {
 				JOptionPane.showMessageDialog(null, "탈출에 성공했습니다. 총 이동 횟수 : " + count);
-				
-			// 랭킹 업로드 메소트
-			LogManager log = new LogManager();	
-			if (count < log.getminCount("woolin")) {
-				log.putLog("woolin", count);
+
+				// 랭킹 업로드 메소드
+				LogManager log = new LogManager();
+				int mincount = log.getMinCount("woolin");
+//				System.out.println(mincount);
+				if (count < mincount || mincount < 0) {
+					log.putLog("woolin", count);
+				}
+
+				finished = true;
 			}
-				
-			finished = true;
-			}
-			
+
 			i++;
 		}
 
 	}
-	
+
+	class ShowRanking extends JFrame {
+		public ShowRanking() {
+			LogManager log = new LogManager();
+			ArrayList<String> rankList = log.getRankingList(); 
+			
+			String[] column = { "Rank", "Mouse name", "Record time", "Moves" };
+			String[][] row = new String[rankList.size()][4];
+			for (int i = 0; i < rankList.size(); i++) {
+				String listline = rankList.get(i);
+				String[] line = listline.split(",");
+				row[i][0] = String.valueOf(i + 1);
+				for (int j = 1; j < 4; j++) {
+					row[i][j] = line[j];
+				}
+			}
+			setTitle("랭킹보기");
+			DefaultTableCellRenderer dtcr = new DefaultTableCellRenderer();
+			dtcr.setHorizontalAlignment(SwingConstants.CENTER);
+
+			DefaultTableModel model = new DefaultTableModel(row, column);
+			JTable table = new JTable(model);
+			table.setRowHeight(25);
+			table.getColumnModel().getColumn(0).setPreferredWidth(20);
+			table.getColumnModel().getColumn(3).setPreferredWidth(20);
+			table.getColumnModel().getColumn(0).setCellRenderer(dtcr);
+			table.getColumnModel().getColumn(3).setCellRenderer(dtcr);
+			JScrollPane sc = new JScrollPane(table);
+			Container c = getContentPane();
+//			String a1 = rankList.get(0);
+//			ranking = new JLabel(a1);
+//			c.add(ranking);
+			c.add(sc);
+			setSize(500, 600);
+			setVisible(true);
+		}
+	}
 
 	public static void main(String[] args) {
 		MazeEscapeGUI me = new MazeEscapeGUI();
-		
+
 		me.loadMap();
 		me.loadMice();
 		me.initWindow();
