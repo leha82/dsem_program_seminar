7029ae7c4bee8c786428fba913308acc4d0ad73f
leha82 <leha82@kunsan.ac.kr>
2020-07-16 16:12:09 +0900

    MazeStorage 및 RandomMapCreator의 설계

--- src/maze/mazemaker/MazeStorage.java
+++ src/maze/mazemaker/MazeStorage.java
@@ -2,4 +2,5 @@ package maze.mazemaker;
 
 public class MazeStorage {
 // Todo : 파일명을 string으로 설정하면 해당 파일을 읽어 DB에 저장
+// 데이터베이스에 저장할 때 폴더명과 확장자는 없애고 순수 파일명만으로 maze이름을 만들 것
 }
--- src/maze/mazemaker/RandomMapCreator.java
+++ src/maze/mazemaker/RandomMapCreator.java
@@ -4,10 +4,13 @@ public class RandomMapCreator {
 	private String mapName;
 	private int x_size;
 	private int y_size;
+	private int start_x, start_y;
+	private int esc_x, esc_y;
+
 	private int[][] map;
 
 	public RandomMapCreator() {
-		this("",0,0);
+		this("", 0, 0);
 	}
 
 	public RandomMapCreator(String mapName, int x_size, int y_size) {
@@ -15,8 +18,49 @@ public class RandomMapCreator {
 		this.x_size = x_size;
 		this.y_size = y_size;
 		this.map = new int[y_size][x_size];
+		
+		this.start_x = 0;
+		this.start_y = 0;
+		this.esc_x = x_size-1;
+		this.esc_y = y_size-1;
 	}
+
+	public void createMap() {
+		// Todo: 여기에서 map을 생성한다.
+	}
+
+	public static void main(String[] args) {
+		RandomMapCreator rmc = new RandomMapCreator("NewMap", 10, 10);
+
+		rmc.createMap();
+		rmc.printMap();
+		rmc.makeMapFile();
+	}
+
+	public void makeMapFile() {
+	// Todo : 맵 파일로 생성 testmap.txt와 동일한 형식으로.
+	// 파일로 저장할때 maps 폴더에 .txt확장자를 붙여서 파일로 저장할 것
+		// this.mapName 활용
 	
+	}
+
+	public void printMap() {
+		String road = "·";
+		String block = "■";
+
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[i].length; j++) {
+				if (map[i][j] == 0) {
+					System.out.print(road);
+				} else if (map[i][j] == 1) {
+					System.out.print(block);
+				}
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+
 	public String getMapName() {
 		return mapName;
 	}
@@ -41,6 +85,38 @@ public class RandomMapCreator {
 		this.y_size = y_size;
 	}
 
+	public int getStart_x() {
+		return start_x;
+	}
+
+	public void setStart_x(int start_x) {
+		this.start_x = start_x;
+	}
+
+	public int getStart_y() {
+		return start_y;
+	}
+
+	public void setStart_y(int start_y) {
+		this.start_y = start_y;
+	}
+
+	public int getEsc_x() {
+		return esc_x;
+	}
+
+	public void setEsc_x(int esc_x) {
+		this.esc_x = esc_x;
+	}
+
+	public int getEsc_y() {
+		return esc_y;
+	}
+
+	public void setEsc_y(int esc_y) {
+		this.esc_y = esc_y;
+	}
+	
 	public int[][] getMap() {
 		return map;
 	}
@@ -48,33 +124,5 @@ public class RandomMapCreator {
 	public void setMap(int[][] map) {
 		this.map = map;
 	}
-	
-	public void createMap() {
-		
-	}
-	
-	public static void main(String[] args) {
-		RandomMapCreator rmc = new RandomMapCreator("NewMap", 10,10);
-		
-		rmc.createMap();
-		rmc.printMap();
-	}
 
-	public void printMap() {
-		String road = "·";
-		String block = "■";
-		
-		for (int i = 0; i < map.length; i++) {
-			for (int j = 0; j < map[i].length; j++) {
-				if (map[i][j] == 0) {
-					System.out.print(road);
-				} else if (map[i][j] == 1) {
-					System.out.print(block);
-				} 
-				System.out.print(" ");
-			}
-			System.out.println();
-		}
-	}
-	
 }
