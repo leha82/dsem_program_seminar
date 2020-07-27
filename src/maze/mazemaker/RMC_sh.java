f566cad5846096553bf9721fe2d72703f21044dc
leha82 <leha82@kunsan.ac.kr>
2020-07-27 17:14:34 +0900

    RMC_sh.java 수정 맵 2~3개 추가

--- src/maze/mazemaker/RMC_sh.java
+++ src/maze/mazemaker/RMC_sh.java
@@ -35,17 +35,49 @@ public class RMC_sh extends RandomMapCreator {
 			break;
 		}
 				
-		start_x = tmp_x;
-		start_y = tmp_y;
-		esc_x = width - 1;
-		esc_y = height - 1; 
+		start_x = -1;
+		start_y = -1;
+		esc_x = -1;
+		esc_y = -1; 
 	}
 
 	public static void main(String[] args) {
-		RMC_sh rmc = new RMC_sh("RMCSH01", 100, 100);
+		RMC_sh rmc = new RMC_sh("RMCSHsmpl100", 100, 100);
 		rmc.createMap();
 		rmc.printMap();
 		rmc.makeMapFile();
+	}
+	
+	private void setStartPoint() {
+		for(int i=0; i<x_size || i<y_size; i++) {
+			for (int j=0; j<=i; j++) {
+				if (i<x_size && j<y_size && map[j][i]==0) {
+					start_x = i;
+					start_y = j;
+					return;
+				} else if (j<x_size && i<y_size && map[i][j]==0) {
+					start_x = j;
+					start_y = i;
+					return;
+				}
+			}
+		}
+	}
+
+	private void setEscPoint() {
+		for(int i=1; i<=x_size || i<=y_size; i++) {
+			for (int j=1; j<=i; j++) {
+				if (x_size-i>0 && y_size-j>0 && map[y_size-j][x_size-i]==0) {
+					esc_x = x_size-i;
+					esc_y = y_size-j;
+					return;
+				} else if (x_size-j>0 && y_size-i>0 && map[y_size-i][x_size-j]==0) {
+					esc_x = x_size-j;
+					esc_y = y_size-i;
+					return;
+				}
+			}
+		}
 	}
 	
 	@Override
@@ -56,15 +88,34 @@ public class RMC_sh extends RandomMapCreator {
 			}
 		}
 		
-		dfsGen(start_x, start_y);
+//		dfsGen(tmp_x, tmp_y, -1);
+		simpleDFSGen(tmp_x, tmp_y);
+		setStartPoint();
+		setEscPoint();
+		
+		System.out.println("Start : " + start_x + "," + start_y);
+		System.out.println("Escape : " + esc_x + "," + esc_y);
 	}
 	
-	private int dep = 0;
-	Scanner sc = new Scanner(System.in);
-	
-	private void dfsGen(int x, int y) {
+	private void simpleDFSGen(int x, int y) {
 		int[] dirList = randomDirList();
 		
+		map[y][x] = 0;
+		
+		for (int i=0; i<dirList.length; i++) {
+			if (checkTiles(x+dir_x[dirList[i]], y+dir_y[dirList[i]], dirList[i])) {
+				simpleDFSGen(x+dir_x[dirList[i]], y+dir_y[dirList[i]]);
+			}
+		}
+	}
+	
+	
+//	private int dep = 0;
+//	Scanner sc = new Scanner(System.in);
+	
+	private void dfsGen(int x, int y, int dir) {
+		int[] dirList;
+
 		map[y][x] = 0;
 
 //		if (dep%10 == 0) {
@@ -73,15 +124,20 @@ public class RMC_sh extends RandomMapCreator {
 //			String str = sc.nextLine();
 //		}
 //		dep++;
-		
-		
+
+		if (dir != -1 && checkTileswithCorner(x+dir_x[dir], y+dir_y[dir], dir) ) {
+			dfsGen(x+dir_x[dir], y+dir_y[dir], -1);
+		}
+
+		dirList = randomDirList();
+
 		for (int i=0; i<dirList.length; i++) {
-			if (checkMap(x+dir_x[dirList[i]], y+dir_y[dirList[i]], dirList[i])) {
-				dfsGen(x+dir_x[dirList[i]], y+dir_y[dirList[i]]);
-			} 
+			if (checkTileswithCorner(x+dir_x[dirList[i]], y+dir_y[dirList[i]], dirList[i])) {
+				dfsGen(x+dir_x[dirList[i]], y+dir_y[dirList[i]], dirList[i]);
+			}
 		}
 		
-		dep--;
+//		dep--;
 	}
 	
 	private int[] randomDirList() {
@@ -104,7 +160,25 @@ public class RMC_sh extends RandomMapCreator {
 		return list;
 	}
 	
-	private boolean checkMap(int x, int y, int dir) {
+	private boolean checkTiles(int x, int y, int dir) {
+		// 현재 위치가 범위 밖이면 false
+		if (x<0 || x>x_size-1 || y<0 || y>y_size-1) 
+			return false;
+		
+		for (int i=0; i<dir_x.length; i++) {
+			if (!((dir+2)%4 == i)) {
+				if (x+dir_x[i]>=0 && x+dir_x[i]<=x_size-1 &&
+					y+dir_y[i]>=0 && y+dir_y[i]<=y_size-1) {
+					if (map[y+dir_y[i]][x+dir_x[i]]==0)
+						return false;
+				}
+			}
+		}
+				
+		return true;
+	}
+	
+	private boolean checkTileswithCorner(int x, int y, int dir) {
 		// 현재 위치가 범위 밖이면 false
 		if (x<0 || x>x_size-1 || y<0 || y>y_size-1) 
 			return false;
@@ -129,4 +203,6 @@ public class RMC_sh extends RandomMapCreator {
 				
 		return true;
 	}
+	
+	
 }
